5825909aa6494defd21d44775970addefcf7f637
g0tie <gaultierbegue@outlook.com>
2021-12-15 10:17:18 +0400

    first commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls;
+using System.IO;
+
+namespace emlViewer.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        private string _fileToOpen = "";
+        public string FileToOpen 
+        {
+            get => _fileToOpen;
+            set {
+                _fileToOpen = value;
+            } 
+        }
+
+        public  List<string> text = new List<string>();
+        public async void openFile(Window window)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Veuillez sélectionner un fichier";
+            fileDialog.AllowMultiple = false;
+
+            var file = await fileDialog.ShowAsync(window);
+
+            if (file != null)
+            {
+                FileToOpen = string.Join(" ", file); ;
+            }
+
+            checkFileExtension(FileToOpen);
+        }
+
+        private void checkFileExtension(string filePath)
+        {
+            string extension = filePath.Split(".")[1];
+
+            if (extension != "eml") {
+                FileToOpen = "";
+                Console.WriteLine("Wrong file format");
+            }
+
+            Console.WriteLine(filePath);
+        }
+
+        public void parseFile()
+        {
+             try
+            {
+             
+                string[] lines = System.IO.File.ReadAllLines(FileToOpen);
+
+                // Display the file contents by using a foreach loop.
+                foreach (string line in lines)
+                {
+                    // if (line[0] == " ") {
+                    //     text[text.Count - 1] += $" {line}";
+                    // } else {
+                    //     text.Add(line);
+                    // }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
