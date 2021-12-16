74b119da1fea221d52a5e883b7844f60ead2220a
g0tie <gaultierbegue@outlook.com>
2021-12-16 14:42:53 +0400

    displaying infos on window and start aligning things

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Avalonia.Controls;
 using System.IO;
 using HLIB.MailFormats;
+using ReactiveUI;
 
 namespace emlViewer.ViewModels
 {
@@ -15,9 +16,110 @@ namespace emlViewer.ViewModels
         {
             get => _fileToOpen;
             set {
-                _fileToOpen = value;
+                _fileToOpen = this.RaiseAndSetIfChanged(ref _fileToOpen, value);
             } 
         }
+
+        
+        private string _from = "";
+        public string From 
+        {
+            get => _from;
+            set {
+                _from =  this.RaiseAndSetIfChanged(ref _from, value);
+            } 
+        }
+
+        private string _to = "";
+        public string To 
+        {
+            get => _to;
+            set {
+                _to =  this.RaiseAndSetIfChanged(ref _to, value);
+            } 
+        }
+
+        private string _cc = "";
+        public string Cc 
+        {
+            get => _cc;
+            set {
+                _cc =  this.RaiseAndSetIfChanged(ref _cc, value);
+            } 
+        }
+
+        private string _subject = "";
+        public string Subject 
+        {
+            get => _subject;
+            set {
+                _subject =  this.RaiseAndSetIfChanged(ref _subject, value);
+            } 
+        }
+
+        private string _date = "";
+        public string Date 
+        {
+            get => _date;
+            set {
+                _date =  this.RaiseAndSetIfChanged(ref _date, value);
+            } 
+        }
+
+        private string _received = "";
+        public string Received 
+        {
+            get => _received;
+            set {
+                _received =  this.RaiseAndSetIfChanged(ref _received, value);
+            } 
+        }
+
+        private string _mimeVersion = "";
+        public string MimeVersion 
+        {
+            get => _mimeVersion;
+            set {
+                _mimeVersion =  this.RaiseAndSetIfChanged(ref _mimeVersion, value);
+            } 
+        }
+
+        private string _contentType = "";
+        public string ContentType 
+        {
+            get => _contentType;
+            set {
+                _contentType =  this.RaiseAndSetIfChanged(ref _contentType, value);
+            } 
+        }
+
+        private string _messageId = "";
+        public string MessageID 
+        {
+            get => _messageId;
+            set {
+                _messageId =  this.RaiseAndSetIfChanged(ref _messageId, value);
+            } 
+        }
+
+        private string _body = "";
+        public string Body 
+        {
+            get => _body;
+            set {
+                _body =  this.RaiseAndSetIfChanged(ref _body, value);
+            } 
+        }
+
+        private string _html = "";
+        public string Html 
+        {
+            get => _html;
+            set {
+                _html =  this.RaiseAndSetIfChanged(ref _html, value);
+            } 
+        }
+
 
         // public  List<string> text = new List<string>();
         public async void openFile(Window window)
@@ -82,6 +184,17 @@ namespace emlViewer.ViewModels
             EMLReader reader = new EMLReader(fs);
             fs.Close();
 
+            From = reader.From;
+            To = reader.To;
+            Cc = reader.CC;
+            Subject = reader.Subject;
+            Date = reader.Date.ToString();
+            Received = reader.Received;
+            MimeVersion = reader.Mime_Version;
+            ContentType = reader.Content_Type;
+            MessageID = reader.Message_ID;
+            Body = reader.Body;
+            Html = reader.HTMLBody;
 
         }
         
